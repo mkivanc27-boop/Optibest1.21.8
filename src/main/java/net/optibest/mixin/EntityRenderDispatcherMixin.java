a5ef0323eb1e420b4a28466f724050d1480887ef
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 17:06:00 +0300

    Update EntityRenderDispatcherMixin.java

--- src/main/java/net/optibest/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/net/optibest/mixin/EntityRenderDispatcherMixin.java
@@ -1,17 +1,7 @@
-package net.optibest.mixin;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private <E extends Entity> void onlyPlayers(E entity, double x, double y, double z, float yaw, float tickDelta, CallbackInfo ci) {
-        if (!(entity instanceof PlayerEntity)) { ci.cancel(); }
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void optimizeRange(CallbackInfoReturnable<Boolean> cir) {
+        // Burada mesafe kontrolü ekleyebiliriz (Örn: 32 blok ötesini renderlama)
     }
 }
-
