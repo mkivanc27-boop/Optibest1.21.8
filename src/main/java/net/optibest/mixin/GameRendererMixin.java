e63ac7767100336acc187b9a507afdeb5b710bb0
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 16:58:32 +0300

    Create GameRendererMixin.java

---
+++ src/main/java/net/optibest/mixin/GameRendererMixin.java
@@ -0,0 +1,13 @@
+package net.optibest.mixin;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "renderBlur", at = @At("HEAD"), cancellable = true)
+    private void noBlur(CallbackInfo ci) { ci.cancel(); }
+}
+
