3004e55b9aa871653a3024f63272f62e22dae8cf
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 17:04:39 +0300

    Update ChunkBuilderMixin.java

--- src/main/java/net/optibest/mixin/ChunkBuilderMixin.java
+++ src/main/java/net/optibest/mixin/ChunkBuilderMixin.java
@@ -1,17 +1,5 @@
-package net.optibest.mixin;
-
-import net.minecraft.client.render.chunk.ChunkBuilder;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 @Mixin(ChunkBuilder.class)
 public class ChunkBuilderMixin {
-    @Inject(method = "rebuild", at = @At("HEAD"), cancellable = true)
-    private void stopRebuild(CallbackInfo ci) {
-        // Chunk'ları sürekli yeniden oluşturma (Gereksiz CPU yükü)
-        ci.cancel();
-    }
+    @ModifyConstant(method = "rebuild", constant = @Constant(intValue = 2))
+    private int fasterChunkRebuild(int original) { return 1; }
 }
-
